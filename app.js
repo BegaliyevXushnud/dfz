04fe9ed485f02225b6ae96c577bf2454ebca9731
BegaliyevXushnud <xushnudpubgmn1@gmail.com>
2024-07-04 17:00:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+
+
+
+// 1-masala
+
+// const nums = [4, 5, 6, 7, 0, 1, 2];
+// const target = 0;
+// const result = nums.indexOf(target);
+//
+// console.log(result);
+
+
+
+
+
+// 2-masala
+// const nums = [1, 3, 2, 4, 5, 6, 99, 102];
+// const target = 5;
+// const result = nums.indexOf(target);
+//
+// console.log(result);
+//
+//
+
+
+
+// 3-masala
+// let obj = {name:"Jack", age:"18", height:"170cm", lastname:"doe",}
+// for(let key in obj){
+//     console.log(key)
+// }
+// for(let key in obj){
+//     console.log(obj[key])
+// }
+
+
+
+
+// 4-masala
+// let objes  = {name: "Mikl", age: "28", height: "160 cm", lastname: "Jackson"};
+// let obj = {name: "Jack", age: "18", height: "170 cm", lastname: "Doe"};
+//
+// let  birlashmaobj = Object.assign({}, obj, objes);
+//
+//
+// for (let key in  birlashmaobj) {
+//     console.log(key);
+// }
+//
+// for (let key in  birlashmaobj) {
+//     console.log( birlashmaobj[key]);
+// }
+
+
+
+// 5-masalsa
+// function Numbers(...numbers) {
+//     let sum = 0;
+//     for (let num of numbers) {
+//         sum += num;
+//     }
+//     return sum;
+// }
+//
+// const result = Numbers(3, 5, 7);
+// console.log('Natija :', result);
+
+
+
+// 6-masala
+//  let arr = [1,4,2,5,3,8,6,9]
+// function count(n) {
+//     arr.sort(function(a,b){return a - b})
+//     console.log(arr);
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] == n) {
+//             console.log(i);
+//         }
+//     }
+//     arr.sort(function(a,b){return b - a})
+//     console.log(arr);
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] == n) {
+//             console.log(i);
+//         }
+//     }
+// }
+// count(3)
